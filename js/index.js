5bba749945017a4524f6c1116f6049935ca006f7
samandargofurov <samandargofurov236@gmail.com>
2024-02-22 20:19:24 +0500

    7lesson

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
+import { getData } from "./functions,js";
+
 const name = document.getElementById('name');
 const surname = document.getElementById('surname');
 const age = document.getElementById('age');
 const email = document.getElementById('email');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
+const logout = document.getElementById('logout');
 
 let fullUrl = window.location.href
 let index = fullUrl.search('index');
@@ -18,8 +21,23 @@ document.addEventListener('DOMContentLoaded', function() {
             return el.username == activeUser
         })
 
-        console.log(user);
+        if (user) {
+            name.innerHTML = user.name
+            surname.innerHTML = user.surname
+            age.innerHTML = user.age
+            email.innerHTML = user.email
+            username.innerHTML = user.username
+            password.innerHTML = user.password
+        } else {
+            window.location.assign(`${baseUrl}pages/login.html`);
+        }
+
     } else {
         window.location.assign(`${baseUrl}/pages/login.html`)
     }
-})
+});
+
+logout && logout.addEventListener('click', function() {
+    localStorage.removeItem('activeUser');
+    window.location.assign(`${baseUrl}pages/login.html`);
+});
